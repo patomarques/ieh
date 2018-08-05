b3c74c9b86765ae68840bf14018b5aa38bcdc206
Pato Marques <patoxmarques@gmail.com>
2018-08-05 08:57:59 -0300

    header - implementado menu fixo (fix top when scroll)

--- wp-content/themes/ieh/custom/js/main.js
+++ wp-content/themes/ieh/custom/js/main.js
@@ -9,4 +9,25 @@ jQuery(document).ready(function($){
         draggable: true,
         pauseOnHover: true
     });
+
+    //menu fixed automatic when scroll
+    window.onload = function() { watcherToFixedMenu() };
+    window.onscroll = function() { watcherToFixedMenu() };
+
+    var next_section     = document.getElementById('home-quem-somos');
+    var menu_fixed       = document.getElementById('menu-home-content');
+    var menu_fixed_col   = document.getElementById('menu-fixed-col');
+    var contentSize      = next_section.offsetTop;
+
+    function watcherToFixedMenu() {
+        if (window.pageYOffset > (contentSize + 20)) {
+            menu_fixed.classList.add("menu-fixed");
+            menu_fixed_col.classList.add('col-lg-10');
+            menu_fixed_col.classList.remove('col-lg-12');
+        } else {
+            menu_fixed.classList.remove("menu-fixed");
+            menu_fixed_col.classList.remove('col-lg-10');
+            menu_fixed_col.classList.add('col-lg-12');
+        }
+    }
 });
