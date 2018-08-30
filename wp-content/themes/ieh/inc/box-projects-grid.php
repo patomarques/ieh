8132e7f5a5c2d8981c66f15b45ec06ad9ef7355e
Pato Marques <patoxmarques@gmail.com>
2018-08-30 04:23:06 -0300

    add inc

--- wp-content/themes/ieh/inc/box-projects-grid.php
+++ wp-content/themes/ieh/inc/box-projects-grid.php
@@ -1,49 +1,49 @@
 <SquareGrid>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/01_exposicoes.png" alt="" class="img-icon">
             <span class="text-box">Exposições</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/02_arte-educacao.png" alt="" class="img-icon">
             <span class="text-box">Arte e educação</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/03_cursos.png" alt="" class="img-icon">
             <span class="text-box">Cursos</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/04_teatro.png" alt="" class="img-icon">
             <span class="text-box">Teatro</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/05_simposios.png" alt="" class="img-icon">
             <span class="text-box">Simpósios</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/06_oficinas.png" alt="" class="img-icon">
             <span class="text-box">Oficinas</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/07_mentoria-grupos.png" alt="" class="img-icon">
             <span class="text-box">Mentoria de grupos</span>
         </div>
     </div>
     <div class="item">
         <div class="square-content">
-            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/nossos-projetos/puzzle.png" alt="" class="img-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/custom/img/icons/box-pic/08_publicacoes.png" alt="" class="img-icon">
             <span class="text-box">Publicações de artistas locais</span>
         </div>
     </div>
