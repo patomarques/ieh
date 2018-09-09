13bd5c78d90bbc99f96cde0715bf5d99d9b392ab
Pato Marques <patoxmarques@gmail.com>
2018-09-09 18:52:01 -0300

    menu-fixed in new file

---
+++ wp-content/themes/ieh/custom/js/menu_fixed.js
@@ -0,0 +1,35 @@
+function watcherToFixedMenu() {
+    var next_section     = document.getElementById('home-quem-somos');
+    var menu_fixed       = document.getElementById('menu-home-content');
+    var menu_fixed_col   = document.getElementById('menu-fixed-col');
+    var page_content     = document.getElementById('page-content');
+//    var contentSize      = page_content.offsetTop;
+
+    //menu fixed automatic when scroll
+    if(typeof(next_section) != 'undefined' && next_section != null){
+        contentSize      = next_section.offsetTop;
+        window.onload = function() { watcherToFixedMenu(contentSize) };
+        window.onscroll = function() { watcherToFixedMenu(contentSize) };
+    }
+
+    if(typeof(page_content) != 'undefined' && page_content != null){
+        contentSize      = page_content.offsetTop;
+        window.onload = function() { watcherToFixedMenu(contentSize) };
+        window.onscroll = function() { watcherToFixedMenu(contentSize) };
+    }
+
+    if (window.pageYOffset > (contentSize + 20)) {
+        menu_fixed.classList.add("menu-fixed");
+        menu_fixed_col.classList.add('col-lg-10');
+        menu_fixed_col.classList.remove('col-lg-12');
+    } else {
+        menu_fixed.classList.remove("menu-fixed");
+        menu_fixed_col.classList.remove('col-lg-10');
+        menu_fixed_col.classList.add('col-lg-12');
+    }
+}
+
+window.onscroll = function (oEvent) {
+    // executa um bloco de código ou funções, quando o scroll é feito na janela.
+    watcherToFixedMenu();
+}
